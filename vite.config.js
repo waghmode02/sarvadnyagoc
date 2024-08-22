cdc8ba9b6e9148bf3ca298b03e459694b8053bb2
waghmode02 <waghmodenagesh84@gmail.com>
2024-08-23 00:04:16 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { join } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure that Vite serves your app from the root
-  build: {
-    outDir: 'dist', // Output directory for the build files
-    rollupOptions: {
-      input: {
-        main: join(__dirname, 'index.html') // Main entry point for the build
-      }
-    }
-  }
-});
+  base: '/',
+})
